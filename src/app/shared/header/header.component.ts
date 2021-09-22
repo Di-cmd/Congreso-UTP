55e5ae01496a4ecc54047ba7e4192e1c3e392938
Diana Salazar <dianac.salazarg@gmail.com>
2021-09-21 19:00:40 -0500

    cambiando el header y el footer

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild,OnInit } from '@angular/core';
+import{SwiperContainer} from '../header/swiper.components'
+
+
+declare var Swiper: any;
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  name = 'Angular with Swiper';
+  @ViewChild('homeSlide', {static: true}) public homeSlide: SwiperContainer;
+
+  public swipeOptions = {
+    spaceBetween: 0,
+    loop: true,
+    speed: 1000,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  };
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  goToNextPage() {
+    this.homeSlide.swiper.slideNext();
+  }
 }
