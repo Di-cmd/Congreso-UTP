ef6c297ec0524c6acbd1b022f7774695728bead3
danpv95 <danielpava95@utp.edu.co>
2021-10-21 16:30:06 -0500

    Inicializacion de modo admn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
-
-
-
 const routes: Routes = [
   {
     path:'',
     loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule)
-    
-
   },
-  
-  
-
 ];
 
 @NgModule({
